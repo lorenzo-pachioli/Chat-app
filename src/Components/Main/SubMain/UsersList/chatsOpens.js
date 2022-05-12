a189d39bfecc180e55f0f71cbf6196efcda86d0f
lorenzo-pachioli <lorenzopachioli@gmail.com>
2022-05-12 18:52:44 -0300

    temporary users for chats opens

---
+++ src/Components/Main/SubMain/UsersList/chatsOpens.js
@@ -0,0 +1,28 @@
+import user from  '../../../../assets/user.png';
+
+export const chats = [
+    {
+        name:'Agent Rue',
+        status:'online',
+        img: user,
+        messages: 1
+    },
+    {
+        name:'Agent Daniel',
+        status:'offline',
+        img: user,
+        messages: 5
+    }, 
+    {
+        name:'Agent William',
+        status:'offline',
+        img: user,
+        messages: 10
+    }, 
+    {
+        name:'Agent Rose',
+        status:'online',
+        img: user,
+        messages: 14
+    }
+]
