051f469551e94e237e35aad07d391b5325d0c0d6
lorenzo-pachioli <lorenzopachioli@gmail.com>
2022-05-16 16:34:11 -0300

    new global states

--- src/Context/AppContext.jsx
+++ src/Context/AppContext.jsx
@@ -4,12 +4,36 @@ export const AppContext = React.createContext();
 
 export default function AppProvider({ children }) {
     const[user, setUser] = useState({})
+    const[userList, setUserList] = useState({})
+    const[token, setToken] = useState({})
+    const[chats, setChats] = useState([])
+    const[messages, setMessages] = useState([])
+    const[redirect, setRedirect] = useState(false);
+    const[loading, setLoading] = useState(false);
+    const[logOut, setLogOut] = useState(false);
+    const [newChat, setNewChat] = useState(false)
 
     return (
         <AppContext.Provider
           value={{
               user,
-              setUser
+              setUser,
+              token,
+              setToken,
+              chats, 
+              setChats, 
+              messages, 
+              setMessages, 
+              userList, 
+              setUserList, 
+              redirect, 
+              setRedirect, 
+              loading, 
+              setLoading, 
+              logOut, 
+              setLogOut, 
+              newChat, 
+              setNewChat
           }}
         >
           {children}
