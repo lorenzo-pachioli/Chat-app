35b35a8b0c1f44b505bfb38112527e61bea8fd55
lorenzo-pachioli <lorenzopachioli@gmail.com>
2022-05-16 16:32:34 -0300

    name next to date

--- src/Components/Main/SubMain/Chat/Message/Message.jsx
+++ src/Components/Main/SubMain/Chat/Message/Message.jsx
@@ -1,12 +1,32 @@
+import React, {useContext} from 'react';
+import { AppContext } from '../../../../../Context/AppContext';
 import './Message.css';
+import  moment  from 'moment';
 
-export default function Message( {date, content}){
+export default function Message( {user, id, date, content, postedBy, readBy}){
 
+    const {userList,} = useContext(AppContext);
+
+
+    const dateFrom = moment(date).fromNow();
+
+    const name =()=>{
+        if(userList.length > 0){
+            const regEx1 = postedBy.replace('{', '')
+            const regEx2 = regEx1.replace('}', '')
+            const nam = userList.find((u)=> u._id === regEx2);
+            return nam._id === user._id ? ('Me'):(`${nam.firstName + ' ' + nam.lastName}`);
+            
+        }
+        
+    } 
+    name();
 
     return(
-        <div className='Message'>
-            <p>{date}</p>
-            <p>{content}</p>
+        <div className='Message' style={postedBy === user._id ? ({alignItems: 'flex-end'}):({alignItems: 'flex-start'})}>
+            
+            <p className='date'>{name()}, {dateFrom}</p>
+            <p className='content'>{content}</p>
         </div>
     )
 }
