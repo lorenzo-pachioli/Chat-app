c2d43c157d90ce1df971f9481a9e9f3df975514f
lorenzo-pachioli <lorenzopachioli@gmail.com>
2022-05-16 16:36:06 -0300

    open or create new chat on click

--- src/Components/Main/SubMain/UsersList/UserCard/UserCard.jsx
+++ src/Components/Main/SubMain/UsersList/UserCard/UserCard.jsx
@@ -1,22 +1,78 @@
+import React, {useContext} from 'react';
+import { AppContext } from '../../../../../Context/AppContext';
+import axios from 'axios';
 import './UserCard.css';
 
-export default function UserCard( {name, status, img, messages}){
+export default function UserCard( {id, status, img, userMessages, chatId, firstName, lastName}){
+
+    const {user, token, setMessages, setChats, userList, newChat, setNewChat, chats, messages} = useContext(AppContext);
+
+    const dateFrom =(date)=>{
+        return new Date(date).getTime()
+    }
 
     const background = 'linear-gradient(178.18deg, #FD749B -13.56%, #281AC8 158.3%)';
+    const name =()=>{
+        if(userList.length > 0){
+            const regEx1 = id.replace('{', '')
+            const regEx2 = regEx1.replace('}', '')
+            const nam = userList.find((u)=> u._id === regEx2);
+            return `${nam.firstName + ' ' + nam.lastName}`;
+        }
+        
+    } 
+
+    const handleMessages = async ()=>{
+        if(newChat){
+            setNewChat(false)
+            await axios.post('https://novateva-codetest.herokuapp.com/room/initiate',{
+                    "userIds": [`{${user._id}}`,`${id}`],
+                    "type": "consumer-to-consumer"
+                },{
+                    headers:{
+                        'Authorization' : `Bearer ${token.auth}`
+                    }
+                })
+                .then(response => console.log(response))
+                .catch(error=> console.error(error))
+
+            await axios.get('https://novateva-codetest.herokuapp.com/room', {headers:{'Authorization' : `Bearer ${token.auth}`}})
+            .then(response => setChats(response.data.conversation))
+        }else{
+            if(id !== user._id){
+                await userMessages.sort((a, b)=>{return dateFrom(a.createdAt) < dateFrom(b.createdAt) })
+                
+                setMessages({
+                    chatId: chatId, 
+                    userMessages: userMessages
+                })
+                await axios.put(`https://novateva-codetest.herokuapp.com/room/${chatId}/mark-read`,{},{
+                    headers:{
+                        'Authorization' : `Bearer ${token.auth}`
+                    }
+                })
+                .then(response => console.log('read', response))
+                .catch(error=> console.error(error))
+                
+            }
+        }
+        
+        
+    }
 
     return(
-        <div className='userCard'>
+        <div className='userCard' onClick={handleMessages}>
             <div className='sub-userCard'>
                 <div className='profile-img' ><span className='dot' style={{backgroundColor:`${status === 'online'?('#8CEE5D'):('#DEDEDE')}`}} /></div>
                 <div className='name'>
-                    <p>{name}</p>
+                    {userList ? (userList.length > 0 ? (name()):('Loading...')):('Loading...')}
                     <p>{status}</p>
                 </div>
 
             </div>
            
-            <p className='msj-number' style={{background:`${messages > 0?(background):('white')}`}} >
-                {messages}
+            <p className='msj-number' style={{background:`${userMessages > 0?(background):('white')}`}} >
+                {id === user._id ? (''):(userMessages.length)}
             </p>
             
             
