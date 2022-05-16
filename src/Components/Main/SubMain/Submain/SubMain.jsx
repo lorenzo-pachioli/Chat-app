84a6495cc43a4823ff98fd1ed7f6066b2c5ae1ea
lorenzo-pachioli <lorenzopachioli@gmail.com>
2022-05-16 16:40:18 -0300

    get all conversations

--- src/Components/Main/SubMain/Submain/SubMain.jsx
+++ src/Components/Main/SubMain/Submain/SubMain.jsx
@@ -1,8 +1,26 @@
+import React, {useContext, useEffect} from 'react';
+import { AppContext } from '../../../../Context/AppContext';
 import Chat from '../Chat/Chat';
 import UserList from '../UsersList/UserList';
+import axios from 'axios';
 import './Submain.css';
 
 export default function SubMain(){
+
+    const { token, setChats} = useContext(AppContext);
+
+    useEffect(() => {
+        
+        async function getConversations(){
+            axios.get('https://novateva-codetest.herokuapp.com/room', {headers:{'Authorization' : `Bearer ${token.auth}`}})
+            .then(response => setChats(response.data.conversation))
+        }
+        getConversations()
+    }, [token, setChats]);
+
+    
+        
+    
 
     return(
         <div className='sub-main'>
