6ee4002cc6baa4235b54bbd3e12602d4306ccb4c
lorenzo-pachioli <lorenzopachioli@gmail.com>
2022-05-16 16:40:42 -0300

    log out

--- src/Components/Main/Main/Main.jsx
+++ src/Components/Main/Main/Main.jsx
@@ -1,8 +1,16 @@
+import React, {useEffect, useContext} from 'react';
+import { AppContext } from '../../../Context/AppContext';
 import SubMain from '../SubMain/Submain/SubMain';
 import ControlPanel from '../ControlPanel/ControlPanel';
 import './Main.css';
 
 export default function Main(){
+    const { setRedirect, setLoading} = useContext(AppContext);
+
+    useEffect(() => {
+        setLoading(false)
+        setRedirect(false);
+    }, [setRedirect, setLoading]);
 
     return(
         <div className='main'>
